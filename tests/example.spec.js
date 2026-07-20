2ec0d2011ec4ef3e8bfa6b2b133e109f71fac3b0
kedar <yadav.kedar31@gmail.com>
2026-07-20 20:14:37 +0530

    First Commit

---
+++ tests/example.spec.js
@@ -0,0 +1,12 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+
+
+  await page.goto('https://google.com/');
+
+  // Expect a title "to contain" a substring.
+  //keda  //Kedar
+  //klklklklk
+})
